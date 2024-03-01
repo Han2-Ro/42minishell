74b3447824a3de42441b1424272ce81d263da0a4
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-03-01 13:49:53 +0100

    add function to find cmd bins

---
+++ src/get_bin.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_bin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 13:46:04 by hrother           #+#    #+#             */
+/*   Updated: 2024/03/01 13:46:29 by hrother          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+char	**getpath(char **envp)
+{
+	while (*envp != NULL)
+	{
+		if (ft_strncmp(*envp, "PATH=", 5) == 0)
+			return (ft_split(*envp + 5, ':'));
+		envp++;
+	}
+	return (NULL);
+}
+
+char	*build_path(char *folder, char *cmd)
+{
+	char	*tmp;
+	char	*res;
+
+	tmp = ft_strjoin(folder, "/");
+	res = ft_strjoin(tmp, cmd);
+	free(tmp);
+	return (res);
+}
+
+char	*path_to_bin(char **folders, char *cmd)
+{
+	char *path;
+
+	if (access(cmd, X_OK) == 0)
+		return (ft_strdup(cmd));
+	while (folders && *folders)
+	{
+		path = build_path(*folders, cmd);
+		if (access(path, X_OK) == 0)
+			return (path);
+		free(path);
+		folders++;
+	}
+	ft_printf("%s: command not found\n", cmd);
+	return (NULL);
+}
