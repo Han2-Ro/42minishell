efad78acdeb7b6baf33b79853601de2c049f4ebb
aprevrha <aprevrha@gmail.com>
2024-03-03 18:39:32 +0100

    added history, make sure to clear it on exit

--- src/loop.c
+++ src/loop.c
@@ -6,7 +6,7 @@
 /*   By: aprevrha <aprevrha@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 15:59:50 by aprevrha          #+#    #+#             */
-/*   Updated: 2024/03/03 17:27:42 by aprevrha         ###   ########.fr       */
+/*   Updated: 2024/03/03 18:24:28 by aprevrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	shell_loop(char *envp[])
 	while (1)
 	{
 		line = readline(prompt);
+		if (line && *line)
+			add_history(line);
 		log_msg(DEBUG, "Inputed line: %s\n", line);
 		run_cmd(line, envp);
 	}
