8dfd167ce539a41689d239e0e5ed8302ac4be6fb
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-03-06 19:48:46 +0100

    add test_exec_builtin

--- src/builtins/exec_buitin.c
+++ src/builtins/exec_buitin.c
@@ -6,7 +6,7 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:36:34 by hrother           #+#    #+#             */
-/*   Updated: 2024/03/06 19:30:20 by hrother          ###   ########.fr       */
+/*   Updated: 2024/03/06 19:37:24 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ int	exec_builtin(const t_cmd cmd)
 {
 	if (ft_strncmp(cmd.bin, "pwd", 5) == 0)
 		exit(builtin_pwd());
-	return (FAILURE);
+	return (SUCCESS);
 }
