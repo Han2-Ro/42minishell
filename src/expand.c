c7f92f48ecb67317c847185c190f913646c847c1
aprevrha <aprevrha@gmail.com>
2024-03-13 17:14:22 +0100

    Quotes working, still needs testing

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: aprevrha <aprevrha@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 13:59:26 by aprevrha          #+#    #+#             */
-/*   Updated: 2024/03/10 23:33:06 by aprevrha         ###   ########.fr       */
+/*   Updated: 2024/03/13 17:05:54 by aprevrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,33 +45,93 @@ char	*str_insert(char const *i_str, char *o_str, unsigned int from,
  * @brief Expands $arg and $? where arg is an environment var
  *
  */
+void	expand_var(unsigned int *i, char *str[1])
+{
+	unsigned int	var_i;
+	char			*temp;
+	char			*env_val;
+
+	var_i = *i;
+	(*i)++;
+	skip_until(*str, i, " $\'\"", true);
+	temp = ft_substr(*str, var_i + 1, *i - var_i - 1);
+	env_val = getenv(temp);
+	if (!env_val)
+		env_val = "";
+	free(temp);
+	temp = str_insert(env_val, *str, var_i, *i);
+	free(*str);
+	*str = temp;
+	*i = var_i + ft_strlen(env_val);
+}
+
+int	get_quote(int quote, const char c)
+{
+	if (quote == 0)
+	{
+		if (c == '\'')
+			return (1);
+		if (c == '"')
+			return (2);
+	}
+	else if (quote == 1)
+	{
+		if (c == '\'')
+			return (0);
+		if (c == '"')
+			return (1);
+	}
+	else if (quote == 2)
+	{
+		if (c == '\'')
+			return (2);
+		if (c == '"')
+			return (0);
+	}
+	return (-1);
+}
+
+void	handle_quote(unsigned int *i, char *str[1], int *quote)
+{
+	int		new_quote;
+	char	*temp;
+
+	new_quote = get_quote(*quote, (*str)[*i]);
+	if (new_quote == -1)
+		log_msg(ERROR, "Quote Error");
+	if (*quote != new_quote)
+	{
+		temp = *str;
+		*str = str_insert("", (*str), *i, *i + 1);
+		free(temp);
+	}
+	else
+		(*i)++;
+	*quote = new_quote;
+}
+
 void	expand(void *token)
 {
 	unsigned int	i;
 	char			*str;
-	char			*temp;
-	unsigned int	var_i;
-	char			*env_val;
+	int				quote;
 
 	str = ((t_token *)token)->value;
+	quote = 0;
 	i = 0;
 	while (str[i] != '\0')
 	{
-		skip_until(str, &i, "$", true);
+		skip_until(str, &i, "$'\"", true);
 		if (str[i] == '\0')
 			break ;
-		var_i = i;
-		i++;
-		skip_until(str, &i, " $\'\"", true);
-		temp = ft_substr(str, var_i + 1, i - var_i);
-		env_val = getenv(temp);
-		if (!env_val)
-			env_val = "";
-		free(temp);
-		temp = str_insert(env_val, str, var_i, i);
-		free(str);
-		str = temp;
-		i = var_i + ft_strlen(env_val);
+		else if (ft_strchr("'\"", str[i]))
+			handle_quote(&i, &str, &quote);
+		else if (str[i] == '$' && quote == 1)
+			i++;
+		else if (str[i] == '$')
+			expand_var(&i, &str);
 	}
+	if (quote != 0)
+		log_msg(WARNING, "Syntax Error: Quote not correctly closed!");
 	((t_token *)token)->value = str;
 }
