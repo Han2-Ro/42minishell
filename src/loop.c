098b4fb29cf90267db1e9818aac117dcbbe81880
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-03-13 18:27:34 +0100

    free cmdlist

--- src/loop.c
+++ src/loop.c
@@ -6,7 +6,7 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 15:59:50 by aprevrha          #+#    #+#             */
-/*   Updated: 2024/03/13 18:06:18 by hrother          ###   ########.fr       */
+/*   Updated: 2024/03/13 18:26:21 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	shell_loop(char *envp[])
 {
-	char *line;
-	char *prompt;
-	t_list *token_lst;
-	t_list *cmd_lst;
+	char	*line;
+	char	*prompt;
+	t_list	*token_lst;
+	t_list	*cmd_lst;
 
 	prompt = "ms>";
-
 	while (1)
 	{
 		line = readline(prompt);
@@ -29,14 +28,12 @@ int	shell_loop(char *envp[])
 			break ;
 		if (line && *line)
 			add_history(line);
-
 		token_lst = lexer(line);
 		if (!token_lst)
 		{
 			log_msg(ERROR, "LEX error");
 			continue ;
 		}
-		free(line);
 		cmd_lst = parse(token_lst, envp);
 		if (!cmd_lst)
 		{
@@ -47,6 +44,8 @@ int	shell_loop(char *envp[])
 			continue ;
 		ft_lstiter(cmd_lst, print_cmd);
 		exec_cmd_list(cmd_lst);
+		free(line);
+		ft_lstclear(cmd_lst, free_cmd);
 	}
 	return (SUCCESS);
 }
