e769b97523a6e5d8f174d6f7957f88eb24aab103
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-03-25 18:16:37 +0100

    add builtin_exit

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,21 +6,23 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 16:00:29 by hrother           #+#    #+#             */
-/*   Updated: 2024/03/25 00:24:49 by hrother          ###   ########.fr       */
+/*   Updated: 2024/03/25 18:14:42 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-int	builtin_env(const t_cmd *cmd, const t_list *envp)
+int	builtin_env(const t_cmd *cmd, t_list **envp)
 {
 	t_env *env;
+	t_list *current;
 
-	while (envp)
+	current = *envp;
+	while (current != NULL)
 	{
-		env = (t_env *)envp->content;
+		env = (t_env *)(current)->content;
 		ft_putendl_fd(env->value, cmd->fd_out);
-		envp = envp->next;
+		current = current->next;
 	}
 	return (SUCCESS);
 }
