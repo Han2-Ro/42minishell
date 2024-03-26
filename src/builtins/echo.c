f64ff081d5f84346e70f1b26984155180b94bb8d
hannes <hannes.rother@online.de>
2024-03-26 13:15:32 +0100

    accept -nnn as flag

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,11 +6,23 @@
 /*   By: hannes <hrother@student.42vienna.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 11:47:08 by hannes            #+#    #+#             */
-/*   Updated: 2024/03/26 12:28:10 by hannes           ###   ########.fr       */
+/*   Updated: 2024/03/26 13:12:58 by hannes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+bool	is_nflag(const char *arg)
+{
+	int	i;
+
+	if (arg[0] != '-' || arg[1] != 'n')
+		return (false);
+	i = 2;
+	while (arg[i] == 'n')
+		i++;
+	return (arg[i] == '\0');
+}
 
 int	builtin_echo(const t_cmd *cmd, t_list **envp)
 {
@@ -19,14 +31,14 @@ int	builtin_echo(const t_cmd *cmd, t_list **envp)
 
 	(void)envp;
 	i = 1;
-	while (cmd->args[i] && ft_strncmp(cmd->args[i], "-n", 5) == 0)
+	while (cmd->args[i] && is_nflag(cmd->args[i]))
 		i++;
 	n_flag = (i > 1);
 	while (cmd->args[i])
 	{
 		ft_putstr_fd(cmd->args[i], cmd->fd_out);
 		if (cmd->args[i + 1])
-			ft_putstr_fd(cmd->args[i], cmd->fd_out);
+			ft_putstr_fd(" ", cmd->fd_out);
 		i++;
 	}
 	if (!n_flag)
