b4df6e4087e3227fd96120a7a206c936b9d6f1b6
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-04-12 14:47:50 +0200

    diff stderr

--- tests/test_against_bash.sh
+++ tests/test_against_bash.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+RUN_DIR=temp
 
-$@ > temp/bash.out
-echo $@ | ./minishell > temp/minishell.out
-diff temp/bash.out temp/minishell.out
+echo $@ | bash > $RUN_DIR/bash.out 2> $RUN_DIR/bash.err
+echo $@ | ./minishell > $RUN_DIR/minishell.out 2> $RUN_DIR/minishell.err
+echo "stdout diff:"
+diff $RUN_DIR/bash.out $RUN_DIR/minishell.out
+echo "stderr diff:"
+diff $RUN_DIR/bash.err $RUN_DIR/minishell.err
