d0f27111e77b7501f16b559491c06d63bd71eb69
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-04-16 18:12:32 +0200

    integrated test_against_bash in make test

--- tests/test_against_bash.sh
+++ tests/test_against_bash.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 RUN_DIR=temp
+FAIL_COUNT=0
 
-< tests/cmds bash > $RUN_DIR/bash.out 2> $RUN_DIR/bash.err
-< tests/cmds ./minishell > $RUN_DIR/minishell.out 2> $RUN_DIR/minishell.err
-echo "stdout diff:"
-diff $RUN_DIR/bash.out $RUN_DIR/minishell.out
-echo "stderr diff:"
-diff $RUN_DIR/bash.err $RUN_DIR/minishell.err
+function test {
+    echo "Test: $1"
+    echo -e $1 | bash > $RUN_DIR/bash.out 2> $RUN_DIR/bash.err
+    exit_bash=$?
+    echo -e $1 | ./minishell > $RUN_DIR/minishell.out 2> $RUN_DIR/minishell.err
+    exit_minishell=$?
+    DIFF=$(diff $RUN_DIR/bash.out $RUN_DIR/minishell.out)
+    if [ "$DIFF" != "" ]; then
+        echo "stdout diff:"
+        echo $DIFF
+        FAIL_COUNT=$((FAIL_COUNT+1))
+    fi
+    DIFF=$(diff $RUN_DIR/bash.err $RUN_DIR/minishell.err)
+    if [ "$DIFF" != "" ]; then
+        # error message may differ
+        if [ $(cat $RUN_DIR/bash.err | wc -l) != $(cat $RUN_DIR/minishell.err | wc -l) ]; then
+            echo "stderr diff:"
+            echo $DIFF
+            FAIL_COUNT=$((FAIL_COUNT+1))
+        fi
+    fi
+    if [ $exit_bash != $exit_minishell ]; then
+        echo "exit code diff:"
+        echo "bash: $exit_bash"
+        echo "minishell: $exit_minishell"
+        FAIL_COUNT=$((FAIL_COUNT+1))
+    fi
+}
+
+test 'nocmd'
+test 'cd .. \n pwd'
+test 'echo $"HOME"$USER'
+exit $FAIL_COUNT
