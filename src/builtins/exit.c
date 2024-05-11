17a47af3c23ceaf49dbfcc1d5d5885c640746ef5
aprevrha <aprevrha@gmail.com>
2024-05-11 16:37:15 +0200

    added bash exit behavior (non numeric)

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,14 +3,66 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: aprevrha <aprevrha@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 17:59:13 by hrother           #+#    #+#             */
-/*   Updated: 2024/04/30 18:03:22 by hrother          ###   ########.fr       */
+/*   Updated: 2024/05/11 16:35:20 by aprevrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static int	ft_isspace(int c)
+{
+	if ((c == ' ') || (c == '\f') || (c == '\n')
+		|| (c == '\r') || (c == '\t') || (c == '\v'))
+		return (1);
+	else
+		return (0);
+}
+
+static int	ft_to_int(char *str)
+{
+	int				i;
+	unsigned int	nbr;
+
+	nbr = 0;
+	i = 0;
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		nbr = nbr * 10 + (str[i] - '0');
+		i++;
+	}
+	return (nbr);
+}
+
+static int	get_exit_status(const char *nptr)
+{
+	char	*str;
+	int		s;
+	int		nbr;
+
+	s = 1;
+	str = (char *)nptr;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			s = -1;
+		str++;
+	}
+	nbr = ft_to_int(str);
+	nbr *= s;
+	while (ft_isdigit(*str))
+		str++;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '\0')
+		return (nbr);
+	log_msg(ERROR, "exit: numeric argument required");
+	return (2);
+}
 
 // TODO: check if the argument is a 8-bit number
 // TODO: free everthing before exit
@@ -27,7 +79,7 @@ int	builtin_exit(const t_cmd *cmd, int status)
 	}
 	if (cmd->args[1] != NULL)
 	{
-		status = ft_atoi(cmd->args[1]);
+		status = get_exit_status(cmd->args[1]);
 	}
 	return (status | EXIT_MASK);
 }
