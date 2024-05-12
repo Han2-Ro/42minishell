8b43eed9ce872f6f0a905906e005aaccc729a760
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-05-12 22:51:43 +0200

    fix tests

--- src/execution/execute_cmd.c
+++ src/execution/execute_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_cmd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: hannes <hrother@student.42vienna.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 16:21:59 by hrother           #+#    #+#             */
-/*   Updated: 2024/05/12 17:53:26 by hrother          ###   ########.fr       */
+/*   Updated: 2024/05/12 22:36:24 by hannes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,23 +48,22 @@ int	try_exec_path(t_cmd *cmd, char **envp_array)
 {
 	struct stat	s_pstat;
 
+	if (cmd->bin == NULL)
+		return (EXIT_MASK | 127);
 	if (stat(cmd->bin, &s_pstat) == -1)
 		log_msg(ERROR, strerror(errno));
 	else if (S_ISDIR(s_pstat.st_mode))
 	{
 		log_msg(ERROR, "Is a Directory");
-		cmd->status = EXIT_MASK | 126;
-		return(126);
+		return (EXIT_MASK | 126);
 	}
 	if (cmd->bin && access(cmd->bin, X_OK) == 0)
 	{
 		execve(cmd->bin, cmd->args, envp_array);
-		log_msg(ERROR, "%s: %s", cmd->bin, strerror(errno));
-		cmd->status = EXIT_MASK | 126;
-		return(126);
+		log_msg(ERROR, "%s: %s, %s:%s", cmd->bin, strerror(errno), __FILE__, __LINE__);
+		return (EXIT_MASK | 126);
 	}
-	cmd->status = EXIT_MASK | 127;
-	return(127);
+	return (EXIT_MASK | 127);
 }
 
 int	exec_cmd(t_cmd *cmd, t_list *cmd_list, t_evars *evars)
@@ -86,7 +85,7 @@ int	exec_cmd(t_cmd *cmd, t_list *cmd_list, t_evars *evars)
 	setup_cmd(cmd, &evars->envp, &envp_array);
 	ft_lstiter(cmd_list, close_fds);
 	log_msg(DEBUG, "executing %s", cmd->bin);
-	try_exec_path(cmd, envp_array);
+	cmd->status = try_exec_path(cmd, envp_array);
 	cmd->status = EXIT_MASK | cmd->status;
 	return (EXIT_FAILURE);
 }
