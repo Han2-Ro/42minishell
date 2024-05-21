dea2029e6f7b8d8a45ecd11a885e06ada8835682
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-05-21 12:54:57 +0200

    check if cmd is path

--- src/execution/get_bin.c
+++ src/execution/get_bin.c
@@ -6,7 +6,7 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 13:46:04 by hrother           #+#    #+#             */
-/*   Updated: 2024/05/14 21:42:56 by hrother          ###   ########.fr       */
+/*   Updated: 2024/05/21 12:43:23 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,21 +34,32 @@ char	*build_path(char *folder, char *cmd)
 	return (res);
 }
 
+char	**get_envpath(t_list *envp)
+{
+	char	*env_path;
+
+	env_path = get_envvalue(envp, "PATH");
+	if (env_path)
+		return (ft_split(env_path, ':'));
+	else
+		return (NULL);
+}
+
 char	*path_to_bin(char *cmd, t_list *envp)
 {
 	char	*path;
-	char	*env_path;
 	char	**folders;
 	int		i;
 
-	if (access(cmd, X_OK) == 0)
-		return (ft_strdup(cmd));
+	if (ft_strchr(cmd, '/') != NULL)
+	{
+		if (access(cmd, X_OK) == 0)
+			return (ft_strdup(cmd));
+		else
+			return (NULL);
+	}
+	folders = get_envpath(envp);
 	i = 0;
-	env_path = get_envvalue(envp, "PATH");
-	if (env_path)
-		folders = ft_split(env_path, ':');
-	else
-		folders = NULL;
 	while (folders && folders[i])
 	{
 		path = build_path(folders[i], cmd);
